5715a8c60c394670bb53111e3e31387559d202ad
HaJunRyu <hjr4557@naver.com>
2022-04-26 18:14:31 +0900

    기본 라우트 설정 (동적 임포트 적용)

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,14 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from 'components/HelloWorld.vue';
-console.log(HelloWorld);
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view />
 </template>
 
 <style lang="scss">
