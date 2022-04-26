ef69293f01a9a30785d51359f1faaad45a0026a1
HaJunRyu <hjr4557@naver.com>
2022-04-26 15:56:57 +0900

    Initial Commit

---
+++ src/components/common/Layout.vue
@@ -0,0 +1,11 @@
+<template>
+  <main><slot></slot></main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  setup() {},
+});
+</script>
