1ad7f9835689ba688b8220f0b0700adfdbbb6fe6
HaJunRyu <hjr4557@naver.com>
2022-04-27 09:47:41 +0900

    주석 제거 및 사용하지 않는 코드 정리

--- src/components/common/Layout.vue
+++ src/components/common/Layout.vue
@@ -2,10 +2,4 @@
   <main><slot></slot></main>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-
-export default defineComponent({
-  setup() {},
-});
-</script>
+<script setup lang="ts"></script>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
 import Home from 'pages/Home.vue';
 
-const routes: Array<RouteRecordRaw> = [
+const routes: RouteRecordRaw[] = [
   {
     path: '/',
     name: 'home',
@@ -10,10 +10,7 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ 'pages/About.vue'),
+    component: () => import('pages/About.vue'),
   },
 ];
 
